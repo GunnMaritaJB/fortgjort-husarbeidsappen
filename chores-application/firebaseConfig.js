f430b1c69c279c629d5a5a4a834a865c5baac8ac
Gunn Marita Jomås <gunnmj18@uia.no>
2025-04-28 19:16:04 +0200

    Added firebase configuration setup

---
+++ chores-application/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAxHkuMO5DUFodi0iBJI2mqDIKIyP6Wx9k",
+    authDomain: "chores-app-f6499.firebaseapp.com",
+    projectId: "chores-app-f6499",
+    storageBucket: "chores-app-f6499.firebasestorage.app",
+    messagingSenderId: "336478453183",
+    appId: "1:336478453183:web:aae9c32e81785726e1f50a",
+    measurementId: "G-1346VWJNBD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
