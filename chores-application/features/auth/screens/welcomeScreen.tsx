72f94f2f704ae3f22bf0b8b6e3ba9326f83cb5a5
Gunn Marita Jomås <gunnmj18@uia.no>
2025-05-08 16:47:44 +0200

    Added async storage to ensure app logs in to household-home page if the user is already logged in and has a household connected to them

--- chores-application/features/auth/screens/welcomeScreen.tsx
+++ chores-application/features/auth/screens/welcomeScreen.tsx
@@ -1,8 +1,35 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebaseConfig';
 import { router } from 'expo-router';
 import { welcomeStyles as styles } from '@/features/auth/styles/welcomeStyles';
 
 export default function WelcomeScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  // Redirect to household screen if user is already logged in
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.replace('/(household)');
+      }
+      else {
+        setIsLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Velkommen til Chores!🧹</Text>
--- chores-application/features/household/screens/landingScreen.tsx
+++ chores-application/features/household/screens/landingScreen.tsx
@@ -1,8 +1,39 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity,  ActivityIndicator } from 'react-native';
+import { getAuth } from 'firebase/auth';
+import { getHouseholdIdForUser } from '@/features/auth/services/authService';
 import { router } from 'expo-router';
 import { landingStyles as styles } from '../styles/landingStyles';
 
 export default function LandingScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      const checkHousehold = async () => {
+        const user = getAuth().currentUser;
+        if (!user) return;
+  
+        const householdId = await getHouseholdIdForUser(user.uid);
+        if (householdId) {
+          router.replace('/(household)/home');
+        }
+        else {
+          setIsLoading(false);
+        }
+      };
+  
+      checkHousehold();
+    }, []);
+
+    if (isLoading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
